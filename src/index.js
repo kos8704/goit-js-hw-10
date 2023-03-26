826855cb7ec854d1b6295887cb8fce64dbcd4171
KOSTIANTYN SHELKOVENKO <kos8704@ukr.net>
2023-03-26 10:57:17 +0300

    update

---
+++ src/index.js
@@ -0,0 +1,91 @@
+import { fetchCountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import './css/styles.css';
+
+const DEBOUNCE_DELAY    = 300;
+const inputSearch       = document.querySelector('#search-box');
+const countriesList     = document.querySelector('.country-list');
+const countryInfo       = document.querySelector('.country-info');
+
+countriesList.hidden    = true;
+countryInfo.hidden      = true;
+
+inputSearch.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
+
+function onInputSearch(event) {
+  event.preventDefault();
+
+  const countryName = inputSearch.value.trim();
+
+  if (!countryName) {
+    clearPage();
+    countriesList.hidden = true;
+    countryInfo.hidden = true;
+    return;
+  }
+
+  fetchCountries(countryName)
+    .then(handleFetchResult)
+    .catch(handleFetchError);
+}
+
+function handleFetchResult(result) {
+  clearPage();
+  if (result.length > 10) {
+    Notiflix.Notify.info('Too many matches found. Please, enter a more specific name.');
+    return;
+  }
+  renderedCountries(result);
+}
+
+function handleFetchError(error) {
+  clearPage();
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+}
+
+function renderedCountries(result) {
+  const numResults = result.length;
+
+  if (numResults > 2 && numResults <= 10) {
+    countryInfo.innerHTML   = '';
+    countryInfo.hidden      = true;
+    countriesList.hidden    = false;
+    renderCountryList(result);
+  } else if (numResults === 1) {
+    countriesList.innerHTML = '';
+    countriesList.hidden    = true;
+    countryInfo.hidden      = false;
+    renderCountryInfoCard(result);
+  }
+}
+
+function renderCountryList(result) {
+  const listMarkup = result
+    .map(({ name, flags }) => {
+      return `
+        <li>
+          <img src="${flags.svg}" alt="${name}" width="50" height="auto">
+          <span>${name.official}</span>
+        </li>`;
+    })
+    .join('');
+  countriesList.innerHTML = listMarkup;
+}
+
+function renderCountryInfoCard(result) {
+  const { flags, name, capital, population, languages } = result[0];
+  const languagesList = Object.values(languages).join(', ');
+  const cardMarkup = `
+    <img src="${flags.svg}" alt="${name}" width="250" height="auto">
+    <h1>${name.official}</h1>
+    <p>Capital: <span>${capital}</span></p>
+    <p>Population: <span>${population}</span></p>
+    <p>Languages: <span>${languagesList}</span></p>`;
+  countryInfo.innerHTML = cardMarkup;
+}
+
+function clearPage() {
+  countriesList.innerHTML = '';
+  countryInfo.innerHTML   = '';
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,15 @@
+
+const URL = 'https://restcountries.com/v3.1/name/';
+const searchParams = new URLSearchParams({
+    fields: 'name,capital,population,flags,languages,',
+});
+
+export const fetchCountries = (name) => {
+    return fetch(`${URL}${name}?${searchParams}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });
+};
